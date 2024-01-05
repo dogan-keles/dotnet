f988461c396a1629394887cd201147c38ea2f7d5
dogan-keles <131661707+dogan-keles@users.noreply.github.com>
2024-01-05 19:18:49 +0300

    Add project files.

---
+++ Controllers/CurrencyController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using WorkintechRestApiDemo.Business;
+using WorkintechRestApiDemo.Domain.ApiLayer;
+using WorkintechRestApiDemo.Domain;
+
+namespace WorkintechRestApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CurrencyController : ControllerBase
+    {
+        protected readonly ICurrencyService currencyService;
+
+        public CurrencyController(ICurrencyService _currencyService)
+        {
+            currencyService = _currencyService;
+        }
+
+
+        [HttpGet(Name = "GetCurrency")]
+        public async Task<CurrencyResponse> Get()
+        {
+
+            return await currencyService.GetCurrency();
+        }
+
+        [HttpPost(Name = "GetCurrencyFromApiLayer")]
+        public async Task<object> Post(string startDate, string endDate)
+        {
+            ApiLayerResponse response = await currencyService.PostCurrencyToApiLayer(startDate, endDate);
+            if (!response.success)
+            {
+                Response.StatusCode = StatusCodes.Status400BadRequest;
+                return BadRequest("Hata oluştu");
+            }
+            return response;
+        }
+    }
+}
